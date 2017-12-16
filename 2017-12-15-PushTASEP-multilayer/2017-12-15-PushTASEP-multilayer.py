b6e284af56424ffe28885a718b6a33694105f45e
Leonid Petrov <lenia.petrov@gmail.com>
2017-12-15 23:10:06 -0500

    will do a simulation of pushtasep, multilayer

---
+++ 2017-12-15-PushTASEP-multilayer/2017-12-15-PushTASEP-multilayer.py
@@ -0,0 +1,31 @@
+#Simulation of the new multilayer PushTASEP
+
+import numpy
+numpy.set_printoptions(threshold=numpy.nan)
+
+print numpy.random.exponential(1)
+
+# 1. Set the parameters
+
+
+
+# 4. print results to txt files
+
+# a = 1 #this is the filename which may be changed; must be matched by "a" in Mathematica file
+#
+# f = open('Hfout' + str(a) + '.txt', 'w')
+#
+# f.write("{")
+# for t in xrange(0,n):
+# 	f.write("{")
+# 	for x in xrange(0,n):
+# 		f.write("{" + str(W[t][x][0]) + "," + str(W[t][x][1]) + ","  + str(W[t][x][2]) + ","  + str(W[t][x][3]))
+# 		if x<n-1:
+# 			f.write("},")
+# 		else:
+# 			f.write("}")
+# 	if t<n-1:
+# 		f.write("},")
+# 	else:
+# 		f.write("}")
+# f.write("}")
