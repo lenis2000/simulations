608c213cd79e70ac86fdc696e4e5871091920fea
Leo Petrov <lenia.petrov@gmail.com>
2018-02-10 13:15:54 -0500

    will do inhomogeneous single layer process simulation

---
+++ 2018-02-10-PushTASEP-single/2017-12-30-PushTASEP-colRSK.py
@@ -0,0 +1,119 @@
+# Simulation of the single layer PushTASEP in inhomogeneous space
+
+import numpy
+numpy.set_printoptions(threshold=numpy.nan)
+
+from PIL import Image, ImageDraw
+
+# 1. Set the parameters
+
+n = 200 #size of the lattice
+t_max = 150 #time till which we simulate
+
+def xi(y):      #inhomogeneous space parameters
+    if y <= 150: 
+        return 1
+    else:
+        return 20
+
+###
+
+a = 1 #output file number
+graph_mult = 10 #the scale at which we display the result
+
+# 2. initialize
+
+P = numpy.ones((k,n)) #array of particles, initially step IC
+XA = numpy.zeros((n)) #array of waiting times per sites
+t = 0 #global time
+
+for x in xrange(0,n):
+    XA[x] = numpy.random.exponential(1./xi(x))
+
+# 3. Some preliminary functions
+
+def who_moves():
+    global t
+    global XA
+    global n
+    global xi
+    j = numpy.argmin(XA)
+    dt = XA[j]
+    t += dt # update global time
+    for xx in xrange(0,n):
+        XA[xx] -= dt
+    XA[j] = numpy.random.exponential(1./xi(j))
+    return j
+
+def make_move(j0):
+    global P
+    global k
+    global n
+
+    l = 0
+    s = j0
+
+    while True:
+        while P[l][s] == 0:
+            l = l + 1
+            if l == k:
+                return
+        P[l][s] = 0
+
+        if s == n - 1:
+            return
+        else: 
+            s = s + 1
+
+        while P[l][s] == 1:
+            s = s + 1
+            if s == n:
+                return
+        P[l][s] = 1
+
+        if l == k - 1:
+            return
+        else:
+            l = l + 1
+        
+
+# 4. Main simulation
+
+m = 0
+while t < t_max:
+    j = who_moves()
+    make_move(j)
+    m += 1
+    if( m % (min(10*n,5000)) == 0):
+        print str(int(t)) + "/" + str(int(t_max))
+
+
+# 5. print results to txt files
+
+f = open('multilayer-pushtasep-RSK-' + str(a) + '.txt', 'w')
+
+f.write("{")
+for i in xrange(0,k):
+    f.write("{")
+    for x in xrange(0,n):
+        f.write(str(int(P[i][x])))
+        if (x<n-1):
+            f.write(",")
+    if (i<k-1):
+        f.write("},")
+    else:
+        f.write("}")
+f.write("}")
+
+# 6. Graphics output
+
+im = Image.new('RGB', (n*graph_mult, k*graph_mult), (255,255,255))
+draw = ImageDraw.Draw(im)
+
+for mi in xrange(0,k):
+    for mx in xrange(0,n):
+        if (int(P[mi][mx]) == 1):
+            draw.rectangle([mx*graph_mult,mi*graph_mult,(mx+1)*graph_mult,(mi+1)*graph_mult], fill="black")
+
+im.show()
+im.save('multilayer-pushtasep-RSK-graph-' + str(a) + '.png')
